4d5f6f5a666f94fba089a57f90f22b580318fcb3
Sushant Kumar Tiwari <sushant22tiwari801@gmail.com>
2024-08-07 22:52:36 +0530

    initial

--- components/Login/LoginScreen.tsx
+++ components/Login/LoginScreen.tsx
@@ -1,11 +1,50 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { ActivityIndicator, StyleSheet, Text, View } from 'react-native'
 import React from 'react'
+import { TouchableOpacity } from 'react-native-gesture-handler'
+import { Ionicons } from 'react-native-vector-icons'
+import { useEffect, useState } from "react";
+import * as Font from "expo-font";
+import {
+  Poppins_300Light,
+  Poppins_400Regular,
+  Poppins_500Medium,
+  Poppins_600SemiBold,
+  Poppins_700Bold,
+} from "@expo-google-fonts/poppins";
+
+const loadFonts = async () => {
+  await Font.loadAsync({
+    "Poppins-Regular": Poppins_400Regular,
+    "Poppins-Bold": Poppins_700Bold,
+    "Poppins-Light": Poppins_300Light,
+    "Poppins-Medium": Poppins_500Medium,
+    "Poppins-SemiBold": Poppins_600SemiBold,
+  });
+};
 
 const LoginScreen = () => {
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+
+  useEffect(() => {
+    loadFonts().then(() => setFontsLoaded(true));
+  }, []);
+
+  if (!fontsLoaded) {
+    return <ActivityIndicator />;
+  }
+  
   return (
     <View style={styles.container}>
-      <Text>LoginScreen</Text>
+      <TouchableOpacity style={styles.backButtonWrapper}>
+        <Ionicons name={"arrow-back-outline"} size={25}/>
+      </TouchableOpacity>
+      <View style={styles.textContainer}>
+        <Text style={styles.headingText}>Hey,</Text>
+        <Text style={styles.headingText}>Welcome</Text>
+        <Text style={styles.headingText}>Back</Text>
+      </View>
     </View>
+
   )
 }
 
@@ -14,6 +53,24 @@ export default LoginScreen
 const styles = StyleSheet.create({
   container:{
     flex:1,
-    padding:24,
-  }
+    backgroundColor: "#fff8ec",
+    marginTop:30,
+    padding: 20,
+  },
+  backButtonWrapper: {
+    height:40,
+    width:40,
+    backgroundColor: "#6F4E37",
+    borderRadius:20,
+    justifyContent:"center",
+    alignItems: "center"
+  },
+  textContainer:{
+    marginVertical:20,
+  },
+  headingText:{
+    fontSize:32,
+    color: "#82330c",
+    fontFamily: "Poppins-SemiBold"
+  },
 })
